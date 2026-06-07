bb358ac9edd3d7a033d613228c3d3c46c83f9702
Gabriele Amoruso <gabryanoru@gmail.com>
2026-06-07 11:35:39 +0200

    feat: update logo references to use IPFS link

--- Yogachain/src/components/navbar.tsx
+++ Yogachain/src/components/navbar.tsx
@@ -32,7 +32,7 @@ export function Navbar() {
             <div className="app-navbar-inner">
                 <div className="app-navbar-row">
                     <div className="app-brand">
-                        <img className="app-brand-icon" src='/YogaLogo.svg' alt="Yoga DApp" />
+                        <img className="app-brand-icon" src='https://red-important-roadrunner-709.mypinata.cloud/ipfs/bafkreigs3qx5cgq2trw2zke63ekkgzjohigd6tzjcfsdzwlmuuppzytgxi' alt="Yoga DApp" />
                         <span className="app-brand-title">Yoga DApp</span>
                     </div>
 
